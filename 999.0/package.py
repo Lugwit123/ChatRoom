835db5cc17992e830196af07ebe7b5cf3696d493
wb.fengqingqing <wb.fengqingqing@mesg.corp.netease.com>
2026-04-22 16:47:16 +0800

    batch push
    rez-package-source automated batch

---
+++ 999.0/package.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+name = "ChatRoom"
+version = "999.0"
+description = "Lugwit ChatRoom: FastAPI/WebSocket backend + React frontend + PySide6 client"
+authors = ["Lugwit Team"]
+
+requires = [
+    "python-3.12+<3.13",
+    "Lugwit_Module",
+    "l_notepad",
+]
+
+build_command = False
+cachable = True
+relocatable = True
+
+
+def commands():
+    env.PYTHONPATH.prepend("{root}/src")
+    # Use Windows-friendly path separators because downstream .bat scripts
+    # frequently concatenate with backslashes.
+    env.CHATROOM_ROOT = "{root}\\src\\ChatRoom"
+
+    alias("chatroom_backend", 'cmd /c "{root}/src/ChatRoom/run_backend_server.bat"')
+    alias(
+        "chatroom_backend_api",
+        'cmd /c "{root}/src/ChatRoom/run_backend_server_api.bat"',
+    )
+    alias("chatroom_frontend", 'cmd /c "{root}/src/ChatRoom/start_frontend.bat"')
+    alias("chatroom_client", 'cmd /c "{root}/src/ChatRoom/run_client.bat"')
+
